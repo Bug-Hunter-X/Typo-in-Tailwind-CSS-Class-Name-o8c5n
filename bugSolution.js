afa49f186c74702f0b7d96afd3740a75fe74cebe
BugHunter-X <askzenai@gmail.com>
2025-02-11 14:26:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,6 @@
+```javascript
+// This component uses Tailwind CSS classes and has the correct class name.
+<div class="bg-blue-500 p-4 text-white text-center">
+  <h1>This is a heading</h1>
+</div>
+```
