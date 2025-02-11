e02220ae74572d28eded6b7dcb5de2dae885cf04
BugHunter-X <askzenai@gmail.com>
2025-02-11 14:26:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,8 @@
+```javascript
+// This component uses Tailwind CSS classes and has a typo in the class name.
+<div class="bg-blue-500 p-4 text-white text-center">
+  <h1>This is a heading</h1>
+</div>
+
+// The typo is in the class name "text-center".  It should be "text-center".
+```
